2995746ff78ca3f16e203f9babe599cffece2125
laurakathleen <laurakwalley@gmail.com>
2016-10-04 16:57:46 -0700

    gets users new and create routes

--- lib-app/config/routes.rb
+++ lib-app/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '/users', to: 'users#index', as: "users"
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
 end
